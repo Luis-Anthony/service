b63e868cbe98f18e5d109748719efd2edcfda1dd
Cesar De Paz <cesardpaz@gmail.com>
2023-06-12 15:10:48 -0500

    Enque style and script

--- public/class-service-public.php
+++ public/class-service-public.php
@@ -7,11 +7,11 @@ class SERVICE_Public {
     }
 
     public function enqueue_styles() {
-        
+        wp_enqueue_style( 'service_style_css', SERVICE_DIR_URI . 'public/css/service_style.css', array(), filemtime(SERVICE_DIR_PATH . 'public/css/service_style.css'), 'all' );
     }
     
     public function enqueue_scripts() {
-        
+        wp_enqueue_script( 'service_script_js', SERVICE_DIR_URI . 'public/js/service_script.js', [], filemtime(SERVICE_DIR_PATH . 'public/js/service_script.js'), true );
     }
 }
 ?>
