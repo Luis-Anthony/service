9cde7a89be24b816b3d66f436cf78983624f1934
Anthony Luis <anthonyluis1730@gmail.com>
2023-06-12 10:53:20 -0500

    Install Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./public/**/*.{php,js}",
+  "./admin/**/*.{php,js}",
+  "./pages/**/*.{php,js}",
+  "./*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
